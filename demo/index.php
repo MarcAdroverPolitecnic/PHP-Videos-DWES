7fb58481a179bf54117bf06ee8db1a7ff51517d6
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-09-26 10:19:31 +0200

    Comit 1 video: Arrays Associatives

--- demo/index.php
+++ demo/index.php
@@ -6,23 +6,26 @@
 </head>
 <body>
 
-    <h1> Llibres Recomenats </h1>
+<h1> Llibres Recomenats </h1>
 
     <?php
         $llibres = [
-            "Do Androids Dream of Electronic Sheep",
-            "The Langoliers",
-            "Hail Mary"
-            ];
+            [
+                'Do Androids Dream of Electric Sheep',
+                'Philip k. Dick',
+                'http://example.com'
+            ],
+            [
+                'Hail Mary',
+                'Andy Wair',
+                'http://example.com'
+            ]
+        ];
     ?>
 
-    <ul>
-        <?php foreach ($llibres as $llibre) : ?>
-            <li> <?= $llibre ?></li>
-
-        <?php endforeach; ?>
-
-    </ul>
+    <p>
+        <?= $llibres[1]?>
+    </p>
 
 </body>
 </html>
