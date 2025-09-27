7c36433e4df6a4c39fa00c07ac65642d1db527e5
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-09-27 15:57:17 +0200

    Comit 1 video: Separar Lògica de la Plantilla

--- demo/index.php
+++ demo/index.php
@@ -1,3 +1,32 @@
+<?php
+$llibres = [
+        [
+                'nom' => 'Do Androids Dream of Electric Sheep',
+                'autor' => 'Philip k. Dick',
+                'anyPublicacio' => '1968',
+                'urlDeCompra' => 'http://example.com'
+        ],
+        [
+                'nom' => 'Hail Mary',
+                'autor' => 'Andy Wair',
+                'anyPublicacio' => '2021',
+                'urlDeCompra' => 'http://example.com'
+        ],
+        [
+                'nom' => 'The Martian',
+                'autor' => 'Andy Wair',
+                'anyPublicacio' => '2011',
+                'urlDeCompra' => 'http://example.com'
+        ]
+];
+
+
+$llibresFiltrats = array_filter($llibres, function ($llibre) {
+    return $llibre['anyPublicacio'] >= 2012;
+});
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,46 +36,6 @@
 <body>
 
 <h1> Llibres Recomenats </h1>
-
-    <?php
-    $llibres = [
-        [
-            'nom' => 'Do Androids Dream of Electric Sheep',
-            'autor' => 'Philip k. Dick',
-            'anyPublicacio' => '1968',
-            'urlDeCompra' => 'http://example.com'
-        ],
-        [
-            'nom' => 'Hail Mary',
-            'autor' => 'Andy Wair',
-            'anyPublicacio' => '2021',
-            'urlDeCompra' => 'http://example.com'
-        ],
-        [
-            'nom' => 'The Martian',
-            'autor' => 'Andy Wair',
-            'anyPublicacio' => '2011',
-            'urlDeCompra' => 'http://example.com'
-        ]
-    ];
-
-        function filtro($items, $fn) {
-
-            $itemsFiltrats = [];
-
-            foreach ($items as $item){
-                if($fn($item)){
-                    $itemsFiltrats[] = $item;
-                }
-            }
-            return $itemsFiltrats;
-        };
-
-        $llibresFiltrats = filtro($llibres, function ($llibre){
-            return $llibre['anyPublicacio'] >= 2012;
-        });
-
-    ?>
 
     <ul>
         <?php foreach ($llibresFiltrats as $llibre) : ?>
