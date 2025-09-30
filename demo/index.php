aadc0943dbe9490024e86b07911bc4133c16d270
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-09-30 09:43:02 +0200

    Comit 1 video: Page Links

--- demo/index.php
+++ demo/index.php
@@ -1,31 +1,4 @@
 <?php
 
-    $llibres = [
-            [
-                    'nom' => 'Do Androids Dream of Electric Sheep',
-                    'autor' => 'Philip k. Dick',
-                    'anyPublicacio' => '1968',
-                    'urlDeCompra' => 'http://example.com'
-            ],
-            [
-                    'nom' => 'Hail Mary',
-                    'autor' => 'Andy Wair',
-                    'anyPublicacio' => '2021',
-                    'urlDeCompra' => 'http://example.com'
-            ],
-            [
-                    'nom' => 'The Martian',
-                    'autor' => 'Andy Wair',
-                    'anyPublicacio' => '2011',
-                    'urlDeCompra' => 'http://example.com'
-            ]
-    ]; //hoala
-
-
-    $llibresFiltrats = array_filter($llibres, function ($llibre) {
-        return $llibre['anyPublicacio'] >= 2012;
-    });
 
     require "index.view.php";
-
-
