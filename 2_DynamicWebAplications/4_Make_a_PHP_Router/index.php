0496b896f10f1bd8a59a7c71f90e8aaf25600377
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-10-01 18:30:31 +0200

    Commit 2 video: Make a PHP Router

--- 2_DynamicWebAplications/4_Make_a_PHP_Router/index.php
+++ 2_DynamicWebAplications/4_Make_a_PHP_Router/index.php
@@ -2,14 +2,4 @@
 
     require 'functions.php';
 
-    $uri = parse_url( $_SERVER['REQUEST_URI'])['path'];
-
-
-    if($uri === '/'){
-        require 'controllers/index.php';
-    } elseif ($uri === '/about'){
-        require 'controllers/about.php';
-    } elseif ($uri === '/contact'){
-        require 'controllers/contact.php';
-    }
-
+    require 'router.php';
