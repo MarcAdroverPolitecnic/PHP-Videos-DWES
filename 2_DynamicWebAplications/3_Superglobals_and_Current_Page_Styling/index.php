f50452686b18eaeb81fd8a4113b1005720b17335
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-10-01 17:00:26 +0200

    Commit 1 video: Superglobals and Current Page Styling

---
+++ 2_DynamicWebAplications/3_Superglobals_and_Current_Page_Styling/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$heading = "Home";
+
+function dd($value){
+
+    echo "<pre>";
+    var_dump($value);
+    echo "<pre>";
+
+    die();
+
+}
+
+    echo $_SERVER['REQUEST_URI'];
+
+    require "views/index.view.php";
