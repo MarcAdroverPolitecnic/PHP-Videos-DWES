b9f21f1f5adf2587bffaa391f064dbf4d0ab84f0
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-10-12 17:14:57 +0200

    Commit 1 video: Resourceful Naming Conventions

---
+++ 4_Project_Organization/1_Resourceful_Naming_Conventions/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes/index.php',
+    '/note' => 'controllers/nots/show.php',
+    '/notes/create' => 'controllers/notes/create.php',
+    '/contact' => 'controllers/contact.php'
+];
