08e0560b40417131cb28cf716806271683c0005d
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-10-15 18:01:49 +0200

    Commit 1 video: Build a Better Router

---
+++ 4_Project_Organization/5_Build_a_Better_Router/Core/Router.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Core;
+class Router {
+    protected $routes = [];
+    public function get($uri, $controller){
+        $this ->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'GET'
+        ];
+    }
+
+    public function post($uri, $controller){
+        $this ->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'POST'
+        ];
+    }
+
+    public function delete($uri, $controller){
+        $this ->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'DELETE'
+        ];
+    }
+
+    public function patch($uri, $controller){
+        $this ->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'PATCH'
+        ];
+    }
+
+    public function put($uri, $controller){
+        $this ->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'PUT'
+        ];
+    }
+
+    public function route($uri){
+        foreach ($this->routes as $route) {
+            if($route['uri'] == $uri){
+                return require base_path($route['controller']);
+            }
+        }
+        $this->abort();
+    }
+
+    protected function abort($code = 404)
+    {
+        http_response_code($code);
+
+        require base_path("views/{$code}.php");
+
+        die();
+    }
+
+}
+
+
+
+
+//function routeToController($uri, $routes) {
+//   if (array_key_exists($uri, $routes)) {
+//       require base_path($routes[$uri]);
+//   } else {
+//       abort();
+//   }
+//
+
+//unction abort($code = 404) {
+//   http_response_code($code);
+
+//   require base_path("views/{$code}.php")  ;
+
+//   die();
+//
+
+$routes = require base_path('routes.php');
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+outeToController($uri, $routes);
