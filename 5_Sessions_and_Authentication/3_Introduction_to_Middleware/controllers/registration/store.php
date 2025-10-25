55431f0f728c08e966adbe5882b311daefc45a70
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-10-25 15:13:00 +0200

    Commit 1 video: Introduction to Middleware

---
+++ 5_Sessions_and_Authentication/3_Introduction_to_Middleware/controllers/registration/store.php
@@ -0,0 +1,55 @@
+<?php
+
+use Core\Database;
+use Core\App;
+use Core\Validator;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+//valaidate the form inputs
+$errors = [];
+if(!Validator::email($email)){
+    $errors['email'] = 'Please provide a valid email address';
+}
+
+if(!Validator::string($password, 7, 255)){
+    $errors['password'] = 'Please provide a valid email password (between 7 and 255 chars';
+}
+
+if(!empty($errors)){
+    return view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
+}
+
+$db = APP::resolve(Database::class);
+
+//chech if the account already exists
+$user = $db ->query('select * from users where email = :email', [
+    'email' => $email
+]) -> find();
+
+if($user){
+    //then someone with that email already exists and has an account
+    //if yes, redirect to a login page
+    header('location: /');
+    exit();
+
+} else{
+    $db ->query('INSERT INTO users(email, password) VALUES (:email, :password)', [
+        'email' => $email,
+        'password' => $password
+    ]);
+
+    //mark that the user has logged in
+    $_SESSION['user'] = [
+        'email' => $email
+    ];
+
+    header('location: /');
+    exit();
+}
+
+    //if yes, redirect to a login page
+    //if not, save one to the database, and then log the user in , and redirect
