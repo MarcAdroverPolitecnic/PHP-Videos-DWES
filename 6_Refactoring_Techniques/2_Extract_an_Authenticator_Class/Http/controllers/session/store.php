1e561f5a2e192415f308ee09941b71599b676f2c
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-11-01 11:16:26 +0100

    Commit 1 video: Extract an Authenticator Class

---
+++ 6_Refactoring_Techniques/2_Extract_an_Authenticator_Class/Http/controllers/session/store.php
@@ -0,0 +1,39 @@
+<?php
+
+use Core\App;
+use Core\Authenticator;
+use Core\Database;
+use Core\Validator;
+use Http\Forms\LoginForm;
+
+
+$db = APP::resolve(Database::class);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$form = new LoginForm();
+
+if($form ->validate(! $email, $password)){
+    return view('session/create.view.php', [
+        'errors' => $form-> errors()
+    ]);
+}
+
+$auth = new Authenticator();
+
+if($auth-> attempt($email, $password)){
+    header('location: /');
+    exit();
+} else {
+    return view('session/create.view.php',[
+        'errors' => [
+            'email' => 'No matching account for that email and password'
+        ]
+    ]);
+}
+
+
+
+
