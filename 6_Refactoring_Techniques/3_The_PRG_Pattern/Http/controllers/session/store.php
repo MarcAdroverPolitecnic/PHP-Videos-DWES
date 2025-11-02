9459f467d6a32e909d00ff06b012a094f13a5e2e
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-11-02 13:41:36 +0100

    Commit 1 video: The PRG Pattern

---
+++ 6_Refactoring_Techniques/3_The_PRG_Pattern/Http/controllers/session/store.php
@@ -0,0 +1,31 @@
+<?php
+
+use Core\Authenticator;
+use Http\Forms\LoginForm;
+
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$form = new LoginForm();
+
+if($form ->validate(! $email, $password)){
+
+    if((new Authenticator())-> attempt($email, $password)){
+        redirect('/');
+    }
+
+    $form->error('email', 'no matching account for that email and password');
+
+}
+
+$_SESSION['_flash']['errors'] = $form->errors();
+
+
+return redirect('/login');
+
+
+
+
+
