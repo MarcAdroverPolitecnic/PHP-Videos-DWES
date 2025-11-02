e95f14502816356818d5a11e6bbb6251be14729a
MarcAdroverPolitecnic <madrover609@alumnes.politecnicllevant.cat>
2025-11-02 17:22:58 +0100

    Commit video: Flash Old From Data to the Session

---
+++ 6_Refactoring_Techniques/4_Flash_Old_Form_Data_To_the_Session/Http/controllers/registration/store.php
@@ -0,0 +1,53 @@
+<?php
+
+use Core\Database;
+use Core\App;
+use Core\Validator;
+
+$db = APP::resolve(Database::class);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+//valaidate the form inputs
+$errors = [];
+if(!Validator::email($email)){
+    $errors['email'] = 'Please provide a valid email address';
+}
+
+if(!Validator::string($password, 7, 255)){
+    $errors['password'] = 'Please provide a valid email password (between 7 and 255 chars';
+}
+
+if(!empty($errors)){
+    return view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
+}
+
+//chech if the account already exists
+$user = $db ->query('select * from users where email = :email', [
+    'email' => $email
+]) -> find();
+
+if($user){
+    //then someone with that email already exists and has an account
+    //if yes, redirect to a login page
+    header('location: /');
+    exit();
+
+} else{
+    $db ->query('INSERT INTO users(email, password) VALUES (:email, :password)', [
+        'email' => $email,
+        'password' => password_hash($password, PASSWORD_BCRYPT)
+    ]);
+
+    //mark that the user has logged in
+    login($user);
+
+    header('location: /');
+    exit();
+}
+
+    //if yes, redirect to a login page
+    //if not, save one to the database, and then log the user in , and redirect
---
+++ 6_Refactoring_Techniques/4_Flash_Old_Form_Data_To_the_Session/Http/controllers/session/store.php
@@ -0,0 +1,35 @@
+<?php
+
+use Core\Authenticator;
+use Http\Forms\LoginForm;
+use Core\Session;
+
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+$form = new LoginForm();
+
+if($form ->validate(! $email, $password)){
+
+    if((new Authenticator())-> attempt($email, $password)){
+        redirect('/');
+    }
+
+    $form->error('email', 'no matching account for that email and password');
+
+}
+
+Session::flash('errors', $form->errors());
+Session::flash('old', [
+    'email' => $_POST['email']
+]);
+
+
+return redirect('/login');
+
+
+
+
+
